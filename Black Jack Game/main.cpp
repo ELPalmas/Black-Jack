ee720bcc17fd3cc16d5428ecfa2b02e318817872
JorgeLeggis <jleggis_23@alu.uabcs.mx>
2024-06-04 16:38:50 -0700

    Update main.cpp

--- Black Jack Game/main.cpp
+++ Black Jack Game/main.cpp
@@ -2,8 +2,57 @@
 
 using namespace std;
 
+int baraja[4][13] = {};
+int manoCrupier = 0, manoJugador = 0;
+
+void generarBaraja();
+void generarCartas();
+
 int main()
 {
-    cout << "Esto es una prueba del github, la cr3eacion y sincrozacion fueron un exito" << endl;
-    return 0;
+    generarBaraja();
+}
+
+void generarBaraja()
+{
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 13; j++)
+        {
+            baraja[i][j] = 1;
+        }
+    }
+
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 13; j++)
+        {
+            cout << "\t" << baraja[i][j];
+        }
+        cout << endl;
+        cout << endl;
+        cout << endl;
+    }
+}
+
+void generarCartas()
+{
+    int x = 0, y = 0;
+    bool seguir = true;
+
+    do
+    {
+        x = rand()% 13;
+        y = rand()% 4;
+
+        if(baraja[y][x] != 0)
+        {
+            baraja[y][x] -=1;
+            seguir = false;
+        }
+    }
+    while(seguir == true);
 }
