1f5e1795a6174cfb8ae5e8c944657f6f69d98cd3
ELPalmas <149444758+ELPalmas@users.noreply.github.com>
2024-06-04 18:54:05 -0700

    Update main.cpp
    se agrego el menu y las reglas del juego

--- Black Jack Game/main.cpp
+++ Black Jack Game/main.cpp
@@ -13,18 +13,48 @@ Proyecto Final
     |||||||||||||
 *******************************************************************************/
 #include <iostream>
+#include <time.h>
+#include <stdlib.h>
 
 using namespace std;
 
+int respuestaInicio;
 int baraja[4][13] = {};
 int manoCrupier = 0, manoJugador = 0;
 
 void generarBaraja();
 void generarCartas();
+void reglasJuego();
 
 int main()
 {
+    cout<<" Bienvenido a Black Jack!"<<endl;
+    cout<<"---------------------------"<<endl;
+    cout<<"[1]Partida Rapida"<<endl;
+    cout<<"[2]Reglamento"<<endl;
+    cout<<"[3]Salir del juego"<<endl;
+    cin>>respuestaInicio;
+
+    switch(respuestaInicio)
+    {
+        case 1:
+            cout<<"Se inicio la partida"<<endl;
+
+            break;
+
+        case 2:
+            reglasJuego();
+            break;
+
+        case 3:
+            cout<<"*Saliste del juego*";
+            exit (0);
+            break;
+
+    }
     generarBaraja();
+
+
 }
 
 void generarBaraja()
@@ -69,4 +99,21 @@ void generarCartas()
         }
     }
     while(seguir == true);
+}
+void reglasJuego()
+{
+    cout<<"1. Inicio del Juego:"<<endl;
+    cout<<"    El crupier reparte dos cartas visibles a cada jugador, incluido él mismo."<<endl;
+    cout<<"    Una de las cartas del crupier está boca arriba (visible) y la otra boca abajo (oculta)."<<endl;
+    cout<<"2. Turno del Jugador:"<<endl;
+    cout<<"    El jugador decide si pide más cartas (""hit"") para acercarse a 21 o se planta (""stand"") con su mano actual."<<endl;
+    cout<<"    Si la suma de las cartas del jugador supera 21, pierde automáticamente (""busto"")."<<endl;
+    cout<<"    El As puede valer 1 u 11 puntos según la mano del jugador."<<endl;
+    cout<<"3. Turno del Crupier:"<<endl;
+    cout<<"    Después de que todos los jugadores hayan actuado, el crupier revela su carta oculta."<<endl;
+    cout<<"    El crupier debe seguir un conjunto de reglas preestablecidas, generalmente pedir cartas hasta alcanzar 17 o más."<<endl;
+    cout<<"4. Determinación del Ganador:"<<endl;
+    cout<<"    Si el jugador tiene una mano más cercana a 21 que el crupier sin pasarse, gana y recibe un pago."<<endl;
+    cout<<"    Si el crupier se pasa de 21 (""busto""), todos los jugadores que no se pasaron ganan automáticamente."<<endl;
+    cout<<"    Si la mano del jugador y la del crupier tienen el mismo valor, es un empate (""push"")."<<endl;
 }
